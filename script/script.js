baf31823c7a3644b4a9a0d54398afb5b82f10a2f
SahasSaurav <60291223+SahasSaurav@users.noreply.github.com>
2020-05-16 12:02:04 +0530

    added animations

--- script/script.js
+++ script/script.js
@@ -2,10 +2,12 @@ const cardTanya=document.getElementById('card-tanya');
 const cardJohn=document.getElementById('card-john');
 const cardBtn=document.querySelectorAll('.card__btn');
 
+
 cardBtn.forEach((btn)=>{
     btn.addEventListener('click',()=>{
         cardTanya.classList.toggle('slide');
         cardJohn.classList.toggle('slide');
+        // img.classList.add('pop');
     })
 })
 
